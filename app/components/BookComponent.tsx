400cc1083f17b43bbf7f7fbb04f67826f9771ce9
sindre <sindre.giske@novari.no>
2025-09-15 14:06:46 +0200

    BookComponent progress

--- app/components/BookComponent.tsx
+++ app/components/BookComponent.tsx
@@ -1,20 +1,29 @@
 import {Box, Button, Heading} from "@navikt/ds-react";
 import type {Book} from "~/types/Book";
+import {BagdeIcon} from "@navikt/aksel-icons";
 
 type BookProps = {
     book: Book;
+    onClick?: () => void;
 };
 
 export default function BookComponent(
-    {book}: BookProps,
+    {book, onClick}: BookProps,
 ) {
 
     return (
         <Box className={"bookCase"}>
-            <Button className={"bookComponent"}>
+            <Button className={"bookComponent"} onClick={onClick}>
                 <Heading align={"center"} size={"small"}>{book.title}</Heading>
                 <Heading align={"center"} size={"xsmall"}>{book.author}</Heading>
             </Button>
+            <div className={"relative"}>
+                {book.loaned ?
+                    <div>{book.loaned}</div>
+                    :
+                    <BagdeIcon title="a11y-title" fontSize="1.5rem"/>
+                }
+            </div>
         </Box>
     )
 }
--- app/routes/main/dashboard.tsx
+++ app/routes/main/dashboard.tsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {Heading, Loader, Page, VStack} from "@navikt/ds-react";
+import {Heading, Label, Loader, Modal, Page, VStack} from "@navikt/ds-react";
 import type {Book} from "~/types/Book";
 import {getAllBooks} from "~/api/BooksAPI";
 import BookComponent from "~/components/BookComponent";
@@ -7,6 +7,7 @@ import BookComponent from "~/components/BookComponent";
 export default function Dashboard() {
     const [allBooks, setAllBooks] = useState<Book[]>([]);
     const [loading, setLoading] = useState(true);
+    const [selectedBook, setSelectedBook] = useState<Book | null>(null);
 
     useEffect(() => {
         const loadBooks = async () => {
@@ -26,17 +27,43 @@ export default function Dashboard() {
     }, [])
 
     return loading ? (
-        <div className={"h-full w-full flex items-center justify-center"}>
+        <div className={"h-full w-full flex items-center justify-center "}>
             <Loader size="3xlarge" title="Loading..."/>
         </div>
     ) : (
-        <Page.Block gutters width={"lg"}>
-            <VStack width={"360px"}>
-                <Heading size="large">All books</Heading>
+        <Page.Block gutters width={"lg"} className={"dashboardBackground"}>
+            <VStack className={"bookShelf"}>
+                <Heading size="large" className={"w-full text-center"}>All books</Heading>
                 {allBooks.map((book) => (
-                    <BookComponent key={book.id} book={book}/>
+                    <BookComponent
+                        key={book.id}
+                        book={book}
+                        onClick={() => setSelectedBook(book)}
+                    />
                 ))}
             </VStack>
+            <Modal
+                width={"small"}
+                open={!!selectedBook}
+                closeOnBackdropClick={true}
+                onClose={() => setSelectedBook(null)}
+                aria-label={selectedBook ? selectedBook.title + " Modal" : "Modal"}
+            >
+                <Modal.Header closeButton className={"text-center"}>
+                    <Heading size={"xlarge"}>{selectedBook?.title}</Heading>
+                    <Heading size={"medium"}>Author: {selectedBook?.author}</Heading>
+                </Modal.Header>
+                <Modal.Body>
+                    {selectedBook?.loaned ?
+                        <Label size={"medium"}>
+                            Book is currently unavailable.
+                        </Label>
+                        :
+                        <div>
+                            
+                        </div>}
+                </Modal.Body>
+            </Modal>
         </Page.Block>
     )
 
